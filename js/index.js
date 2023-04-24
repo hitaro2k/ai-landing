8a46072ccef06abac9c8bad4e663173d5d7b7816
hitaro2k <vlad.bojenko2005@gmail.com>
2023-04-24 15:26:07 +0300

    add form open-close

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@
 let whiteList = document.querySelectorAll("#whitelist")
 let faqList = document.querySelectorAll("#faqlist")
 let whiteListText = document.querySelectorAll(".list-text")
+let formBtn = document.querySelectorAll(".whitelist-button")
+let form  = document.querySelector(".popup")
+let formClose = document.querySelector(".popup__close")
+let doc = document.querySelector("html")
 
 
 function openList(){
+
     whiteList.forEach((item) =>{
         item.addEventListener("click",() => {
            
@@ -19,7 +24,21 @@ function openList(){
 
 }
 
+function openForm(){
+    formBtn.forEach((item) =>{
+        item.addEventListener("click", ()=>{
+            form.style.display = "flex"
+            doc.style.position = "fixed"
+        })
+    })
+    formClose.addEventListener("click", ()=>{
+        form.style.display = "none"
+        doc.style.position = "static"
+    })
+   
+}
 openList()
+openForm()
 function whiteListForm(){
 
 }
