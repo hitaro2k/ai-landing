c77a11fd28adaf689004a1ddc949bc8971badeb7
hitaro2k <vlad.bojenko2005@gmail.com>
2023-04-24 15:08:26 +0300

    point

---
+++ js/index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+let whiteList = document.querySelectorAll("#whitelist")
+let faqList = document.querySelectorAll("#faqlist")
+let whiteListText = document.querySelectorAll(".list-text")
+
+
+function openList(){
+    whiteList.forEach((item) =>{
+        item.addEventListener("click",() => {
+           
+        })
+    })
+    faqList.forEach((item)=>{
+        item.addEventListener("click", ()=>{
+
+        })
+    })
+
+}
+
+openList()
+function whiteListForm(){
+
+}
